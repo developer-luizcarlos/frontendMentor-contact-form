a09c6f3a84779863f123486639b9404106c934cf
luiz <developer.luizcarlos@outlook.com>
2024-08-12 11:44:17 -0300

    refactoring code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,18 +8,6 @@ import InputWrapper from "@/components/inputWrapper/inputWrapper";
 import RadioWrapper from "@/components/radioWrapper/radioWrapper";
 
 export default function Home() {
-  const [nameInputValid, setNameInputValid] = useState<boolean>(false);
-  const [lastNameInputValid, setLastNameInputValid] = useState<boolean>(false);
-  const [emailInputValid, setEmailInputValid] = useState<boolean>(false);
-  const [typeRequisitionInputValid, setTypeRequisitionInputValid] = useState<boolean>(false);
-  const [messageTextAreaInputValid, setMessageTextAreaInputValid] = useState<boolean>(false);
-  const [concernInputValid, setConcernInputValid] = useState<boolean>(false);
-
-  let formName: string = "";
-  let formLastName: string = "";
-  let formEmail: string = "";
-  let formTypeRequisition: string = "";
-  let formMessage: string = "";
   let formConcern: string = "";
 
   return (
@@ -35,11 +23,6 @@ export default function Home() {
             inputType="text"
             errorMessage="This field is required"
             errorState={false}
-            changeFunction={(event) => {
-              const value: string = event.target.value;
-              formName = value;
-              setNameInputValid(true);
-            }}
           />
           <InputWrapper
             labelString="Last Name"
@@ -47,11 +30,6 @@ export default function Home() {
             inputType="text"
             errorMessage="This field is required"
             errorState={false}
-            changeFunction={(event) => {
-              const value: string = event.target.value;
-              formLastName = value;
-              setLastNameInputValid(true);
-            }}
           />
         </div>
         <div className="form_email_wrapper">
@@ -61,31 +39,18 @@ export default function Home() {
             inputType="email"
             errorMessage="Please enter a valid email address"
             errorState={false}
-            changeFunction={(event) => {
-              const value: string = event.target.value;
-              formEmail = value;
-              setEmailInputValid(true);
-            }}
           />
         </div>
         <RadioWrapper
           showError={false} />
         <div className="form_textarea_wrapper">
           <label className="form_textarea_wrapper_label" htmlFor="form_message_user">message</label>
-          <textarea id="form_message_user" className="form_text_wrapper_textarea_normal " onChange={(event) => {
-            const value: string = event.target.value;
-            formMessage = value;
-            setMessageTextAreaInputValid(true);
-          }}></textarea>
-          <small className="form_textarea_small" style={{ display: messageTextAreaInputValid ? "block" : "none" }}>This field is required</small>
+          <textarea id="form_message_user" className="form_text_wrapper_textarea_normal"></textarea>
+          <small className="form_textarea_small">This field is required</small>
         </div>
         <div className="form_box_content">
           <div className="form_box_body">
-            <input type="checkbox" id="form_consent_checkbox" onChange={(event) => {
-              const value = event.target.value;
-              formMessage = value;
-              setConcernInputValid(true);
-            }} />
+            <input type="checkbox" id="form_consent_checkbox" />
             <label className="form_consent_label" htmlFor="form_consent_checkbox">I consent to being contacted by the team</label>
           </div>
         </div>
--- src/components/inputWrapper/inputWrapper.tsx
+++ src/components/inputWrapper/inputWrapper.tsx
@@ -4,7 +4,8 @@ import Styles from './inputWrapper.module.css';
 interface Properties {
     labelString: string;
     inputName: string;
-    inputType: string
+    inputType: string;
+    errorOpacity?: boolean;
     errorMessage: string;
     errorState: boolean;
     changeFunction?:  ChangeEventHandler<HTMLInputElement>;
@@ -14,8 +15,8 @@ export default function inputWrapper(props: Properties) {
     return (
         <div className={Styles.input_wrapper}>
             <label htmlFor={props.inputName}>{props.labelString}</label>
-            <input className={props.errorState ? Styles.error_state : Styles.normal_state} type={props.inputType} id={props.inputName} onChange={props.changeFunction}/>
-            <small style={{ display: props.errorState ? "block" : "none" }}>{props.errorMessage}</small>
+            <input className={props.errorState ? Styles.normal_state : Styles.error_state} type={props.inputType} id={props.inputName} onChange={props.changeFunction}/>
+            <small style={{ opacity: props.errorOpacity ? 0 : 1 }}>{props.errorMessage}</small>
         </div>
     )
 };
